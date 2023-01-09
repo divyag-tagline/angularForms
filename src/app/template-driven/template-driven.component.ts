514332324dc152ffd340aced428c2e187e2b3046
divyag_tagline <divyag.tagline@gmail.com>
2023-01-09 15:09:55 +0530

    add and display opration include in form

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,44 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-interface data {
-  name:'',
-  email:'',
-  age:0
+interface Data {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  mobileNo: string;
+  gender: string;
 }
 @Component({
   selector: 'app-template-driven',
   templateUrl: './template-driven.component.html',
-  styleUrls: ['./template-driven.component.scss']
+  styleUrls: ['./template-driven.component.scss'],
 })
 export class TemplateDrivenComponent implements OnInit {
-  data !:data;
-  form:[]=[]
-  name !: string;
-  email !: string;
-  age !: number;
-  preference !: string;
-  preferences: string[] = ['Cooking', 'Reading', 'Coding'];
-  constructor() { }
-  genders = ['male', 'female'];
-  
-  userData = {
-    username: '',
-    email: '',
-    country: '',
-    gender: ''
-  };
-  submitted = false;
-  ngOnInit(): void {
+  @ViewChild('formdemo') formdemo!: NgForm;
+  // data !: data;
+  details: Data[] = [
+    {
+      id: 1,
+      firstName: 'Divya',
+      lastName: 'Gabani',
+      email: 'divyagabani@gmail.com',
+      mobileNo: '7890123654',
+      gender: 'female',
+    },
+    {
+      id: 2,
+      firstName: 'Grishama',
+      lastName: 'Borda',
+      email: 'grishma@gmail.com',
+      mobileNo: '9874012365',
+      gender: 'female',
+    },
+  ];
 
-  }
+  genders = ['male', 'female'];
+
+  userData: Data = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobileNo: '',
+    gender: '',
+  };
+  constructor() {}
+
+  submitted = false;
+  ngOnInit(): void {}
   // handleSubmit(): void{
   //     console.log(this.name);
   // }
-  onSubmit(form :NgForm,formData : any) {
-    console.log('submitted formdata',formData);  
-    
-    alert('Form submitted successfully');
-    
-    form.reset();
+  blockCharacter(e: any) {
+    // console.log("block character",value);
+    var x = e.which || e.keycode;
+    if (x >= 42 && x <= 57) return true;
+    else return false;
+  }
+
+  onSubmit() {
+    if (this.formdemo.form.invalid) {
+      return;
+    }
+    const data = {
+      id: this.details.length + 1,
+      ...this.formdemo.form.value,
+    };
+    this.details.push(data);
+    this.formdemo.reset();
   }
 }
