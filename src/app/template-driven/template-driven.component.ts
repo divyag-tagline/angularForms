95f96f34f163950deb718384b7c95c39271ba3a7
divyag_tagline <divyag.tagline@gmail.com>
2023-01-10 18:30:07 +0530

    add hobbies feild  in form

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { flush } from '@angular/core/testing';
 import { NgForm } from '@angular/forms';
-import { VirtualTimeScheduler } from 'rxjs';
 interface Data {
   id: number;
   firstName: string;
   lastName: string;
   email: string;
   mobileNo: string;
+  hobby?: Hobbies[];
   gender: string;
-  country:string;
-  state:string;
-  city:string;
+  address: Address;
+}
 
+interface Address {
+  country: string;
+  state: string;
+  city: string;
+}
+interface Hobbies {
+  id?: number;
+  label: string;
+  selected?: boolean;
 }
 @Component({
   selector: 'app-template-driven',
@@ -31,10 +38,12 @@ export class TemplateDrivenComponent implements OnInit {
       lastName: 'Gabani',
       email: 'divyagabani@gmail.com',
       mobileNo: '7890123654',
+      hobby: [
+        { id: 1, label: 'Drawing', selected: true },
+        { id: 2, label: 'Cooking', selected: true },
+      ],
       gender: 'female',
-      country:'India',
-      state:'Gujarat',
-      city:'Surat'
+      address: { country: 'India', state: 'Gujarat', city: 'Surat' },
     },
     {
       id: 2,
@@ -42,17 +51,20 @@ export class TemplateDrivenComponent implements OnInit {
       lastName: 'Borda',
       email: 'grishma@gmail.com',
       mobileNo: '9874012365',
+      hobby: [{ id: 1, label: 'Drawing', selected: true }],
       gender: 'female',
-      country:'Canada',
-      state:'Kerala',
-      city:'Ahmedabad'
+      address: { country: 'Canada', state: 'Kerala', city: 'Ahmedabad' },
     },
   ];
-
+  selectedHobby: string[] = [];
   genders = ['male', 'female'];
-  country = ['India','Canada'];
-  state = ['Gujarat','Kerala'];
-  city = ['Surat','Ahmedabad'];
+  hobbies = [
+    { id: 1, label: 'Drawing', selected: false },
+    { id: 2, label: 'Cooking', selected: false },
+  ];
+  countries = ['India', 'Canada'];
+  states = ['Gujarat', 'Kerala'];
+  cities = ['Surat', 'Ahmedabad'];
   submitted: boolean = false;
   userData: Data = {
     id: 0,
@@ -61,9 +73,8 @@ export class TemplateDrivenComponent implements OnInit {
     email: '',
     mobileNo: '',
     gender: '',
-    country:'',
-    state:'',
-    city:''
+    hobby: [],
+    address: { country: '', state: '', city: '' },
   };
   editData!: Data;
   editId!: number;
@@ -78,7 +89,21 @@ export class TemplateDrivenComponent implements OnInit {
     if (x >= 42 && x <= 57) return true;
     else return false;
   }
-
+  onChangeData(e:any){
+    let selectedValue = e.target.value;
+    console.log(selectedValue);
+    
+    let checked = e.target.checked;
+    if(checked){
+      this.selectedHobby.push(selectedValue)
+    }
+    else{
+      let index = this.selectedHobby.indexOf(selectedValue);
+      this.selectedHobby.splice(index,1)
+    }
+    console.log(this.selectedHobby);
+    
+  }
   onSubmit() {
     // !this.toggle ? event.target.innerText = 'update' : event.target.innerText = 'submit'
     if (this.formdemo.form.invalid) {
@@ -89,19 +114,19 @@ export class TemplateDrivenComponent implements OnInit {
         let values = this.formdemo.form.value;
         console.log('user', this.editId);
         console.log('data');
-            this.details[this.dataId] ={ 
-              id:this.editId,
-              ...this.formdemo.form.value};
-            this.toggle = false;
-            this.editId = 0;
-            // items.firstName=  values.firstName,
-            // items.lastName= values.lastName,
-            // items.email= values.email,
-            // items.mobileNo= values.mobileNo,
-            // items.gender=values.gender
-          
+        this.details[this.dataId] = {
+          id: this.editId,
+          ...this.formdemo.form.value,
+        };
+        this.toggle = false;
+        this.editId = 0;
+        // items.firstName=  values.firstName,
+        // items.lastName= values.lastName,
+        // items.email= values.email,
+        // items.mobileNo= values.mobileNo,
+        // items.gender=values.gender
       } else {
-        console.log('submit');
+        console.log('submit', this.formdemo.form.value);
 
         const data = {
           id: this.details.length + 1,
@@ -120,12 +145,21 @@ export class TemplateDrivenComponent implements OnInit {
     // console.log('detail', this.details);
   }
   handleEdit(data: Data, index: number) {
-    console.log("data",data);
+    console.log('data', data);
     this.formdemo.form.patchValue(data);
     this.submitted = false;
     this.editId = data.id;
     this.dataId = index;
     this.toggle = true;
     this.editData = data;
+  }
+  changeData(currentOption: any, allOptions: any) {
+    // allOptions.map((data) =>
+    //   data.selected ?
+    //   this.selectedHobby = currentOption.label : null
+    // )
+    this.selectedHobby = allOptions;
+    console.log('this.details :>> ', this.selectedHobby);
+    // console.log('All options changed with ngModel', allOptions);
   }
 }
