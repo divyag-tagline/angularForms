e5dc6b9a4ede25edd2b9698d531c0b6dcd1e1c2b
divyag_tagline <divyag.tagline@gmail.com>
2023-01-12 17:03:01 +0530

    Edit optration with all validation in reactive-form

---
+++ src/app/reactive-form/address.service.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@angular/core';
+export interface Country{
+  countryName: string,
+  countryId:number
+}
+export interface State{
+  stateName:string,
+  stateId:number,
+  countryId:number
+}
+export interface City{
+  cityName : string,
+  cityId:number,
+  stateId:number
+}
+@Injectable({
+  providedIn: 'root',
+})
+export class AddressService {
+  country: Country[] = [
+    {
+      countryName: 'India',
+      countryId: 1,
+    },
+    {
+      countryName: 'Canada',
+      countryId: 2,
+    },
+  ];
+  state: State[] = [
+    {
+      stateName: 'Gujarat',
+      stateId: 101,
+      countryId: 1,
+    },
+    {
+      stateName: 'Kerala',
+      stateId: 102,
+      countryId: 1,
+    },
+    {
+      stateName: 'British Colombia',
+      stateId: 103,
+      countryId: 2,
+    },
+    {
+      stateName: 'Ontario',
+      stateId: 104,
+      countryId: 2,
+    },
+  ];
+  city: City[] = [
+    {
+      cityName: 'Surat',
+      cityId: 201,
+      stateId: 101,
+    },
+    {
+      cityName: 'Ahemdabad',
+      cityId: 202,
+      stateId: 101,
+    },
+    {
+      cityName: 'Kochi',
+      cityId: 203,
+      stateId: 102,
+    },
+    {
+      cityName: 'Kottayam',
+      cityId: 204,
+      stateId: 102,
+    },
+  ];
+  constructor() {}
+
+  getStatesByCountry(countryId: number) {
+    console.log(this.state.filter((data)=>data.countryId === +countryId));
+    return this.state.filter((data)=>data.countryId === +countryId);
+  }
+  getCityByState(stateId:number){
+    return this.city.filter((data)=>data.stateId === +stateId);
+  }
+}
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { AddressService, City, Country, State } from './address.service';
 interface UsersDetails {
   id: number;
   firstName: string;
   lastName: string;
   email: string;
+  birthDate: Date;
   mobileNo: string;
   gender: string;
+  address: Address;
+}
+interface Address {
+  country: string;
+  state: string;
+  city: string;
 }
 @Component({
   selector: 'app-reactive-form',
@@ -19,6 +28,13 @@ interface UsersDetails {
   styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent implements OnInit {
+  countries!: Country[];
+  states!: State[];
+  cities!: City[];
+  getStateDetails: any;
+  countryId!: number;
+  stateId!: number;
+  btnDisable: boolean = true;
   genders = [
     {
       name: 'Male',
@@ -35,30 +51,60 @@ export class ReactiveFormComponent implements OnInit {
       firstName: 'divya',
       lastName: 'gabani',
       email: 'divya@gmail.com',
+      birthDate: new Date('06-09-2001'),
       mobileNo: '9874563210',
       gender: 'female',
+      address: { country: 'India', state: 'Gujarat', city: 'Surat' },
     },
   ];
   submitted = false;
   profileForm!: FormGroup;
   editForm!: UsersDetails;
   editId!: number;
-  constructor(private fb: FormBuilder) {}
+  address!: Address;
+  dataId!: number;
+  toggle: boolean = false;
+  selectedState: boolean = false;
+  constructor(private addressService: AddressService) {
+    // console.log('country', this.countryId);
+    this.countries = this.addressService.country;
+  }
 
   ngOnInit(): void {
     this.createProfileForm();
   }
-  get profileFormControl() {
-    return this.profileForm.controls;
+  ngDoCheck(): void {
+    if (this.countryId) {
+      console.log('country', this.countryId);
+      this.states = this.addressService.getStatesByCountry(this.countryId);
+      this.cities = this.addressService.getCityByState(this.stateId);
+    }
+
+    console.log(this.states);
   }
   createProfileForm() {
     this.profileForm = new FormGroup({
       firstName: new FormControl(null, Validators.required),
       lastName: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
+      birthDate: new FormControl('', [Validators.required]),
       mobileNo: new FormControl('', [Validators.required]),
       gender: new FormControl('', [Validators.required]),
+      address: new FormGroup({
+        country: new FormControl('', [Validators.required]),
+        state: new FormControl('', [Validators.required]),
+        city: new FormControl('', [Validators.required]),
+      }),
     });
+  }
+
+  get profileFormControl() {
+    return this.profileForm.controls;
+  }
+
+  get addressControl() {
+    return (this.profileFormControl['address'] as FormGroup).controls;
+    // return (this.profileForm.controls['address'] as FormGroup).controls
   }
 
   blockCharacter(e: any) {
@@ -67,36 +113,58 @@ export class ReactiveFormComponent implements OnInit {
     else return false;
   }
   onSubmit() {
+    console.log(this.profileForm);
+    
     if (this.profileForm.invalid) {
       this.submitted = true;
       return;
     } else {
       if (this.editId) {
-        let editForm = {
+        console.log(this.editId);
+        console.log(this.editForm);
+        console.log(this.usersDetails);
+
+        this.usersDetails[this.dataId] = {
           id: this.editId,
           ...this.profileForm.value,
         };
-        console.log(editForm);
-
-        this.usersDetails[this.editId] = editForm;
+        this.editId = 0;
+        this.toggle = false;
+        console.log('update');
       } else {
         let data = {
           id: this.usersDetails.length + 1,
           ...this.profileForm.value,
         };
+        console.log('data', data);
+
         this.usersDetails.push(data);
       }
     }
+    console.log(this.usersDetails);
     this.profileForm.reset();
     this.submitted = false;
   }
-  handleEdit(data: UsersDetails, id: number) {
-    console.log(data.id);
+  handleEdit(data: UsersDetails, index: number) {
+    // console.log(data.id);
     this.profileForm.patchValue(data);
-    this.editId = data.id;
     this.editForm = data;
+    this.editId = data.id;
+    this.toggle = true;
+    this.dataId = index;
+
+    console.log(this.editId);
+    console.log(index);
   }
   handleDelete(index: number) {
     this.usersDetails.splice(index, 1);
+  }
+  selectCountry(e: any) {
+    console.log(e);
+    this.countryId = e.target.value;  ``
+    this.btnDisable = false;
+  }
+  selectState(e: any) {
+    this.stateId = e.target.value;
   }
 }
